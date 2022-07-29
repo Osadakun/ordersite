2dbdd184f2cbbddb7bc2a2db4dfac7fc63b07542
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-07-29 23:37:08 +0900

    ログイン前の画面の表示に成功

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email])            #まず、送られてきたメースアドレスでユーザーを検索する
 
-      if user&.authenticate(session_params[:password])          #ユーザーが見つかった場合には、送られてきたパスワードによる認証をauthenticateメソッドを使って行います
+      if user&.authenticate(session_params[:password])            #ユーザーが見つかった場合には、送られてきたパスワードによる認証をauthenticateメソッドを使って行います
         session[:user_id] = user.id                               #認証に成功した場合に、セッションにuser_idを格納しています。
 
         redirect_to root_path, notice: 'ログインしました'
